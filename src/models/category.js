f6e2379265cae9472d2098c62a7c281f6a5d973b
Rabiaats <atesr782@gmail.com>
2025-04-18 14:42:00 +0300

    docs: category and brand models

---
+++ src/models/category.js
@@ -0,0 +1,27 @@
+"use strict"
+
+const { mongoose } = require('../configs/dbConnection')
+
+
+const CategorySchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+    },
+    image: {
+            type: String,
+            required: true,
+            trim: true
+        }
+    
+
+}, {
+    collection: 'categories',
+    timestamps: true
+})
+
+
+module.exports = mongoose.model('Category', CategorySchema)
